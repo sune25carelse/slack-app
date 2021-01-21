de23db92933889a203824b3f413ff50a52bb8236
Sune Carelse <sune.carelse@takealot.com>
2021-01-21 20:33:57 +0200

    react-route for switch between channels

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Chat from './Chat';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Fabba Programmer.... lets do the slack clone🚀🚀</h1>
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
