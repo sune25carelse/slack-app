fc52d1dfee1f1b746ee63a970a2fbe6c23b7a89e
Sune Carelse <sune.carelse@takealot.com>
2021-01-21 22:10:31 +0200

    creates db entry then fetch the room id and detail

--- src/Chat.js
+++ src/Chat.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Chat.css';
 import { useParams } from "react-router-dom";
 import { InfoOutlined, StarBorderOutlined } from '@material-ui/icons';
+import db from './firebase';
 
 function Chat() {
     const { roomId } = useParams();
+    const [ roomDetails, setRoomDetails ] = useState(null);
+
+    useEffect(() => {
+        if (roomId) {
+            db.collection ('rooms')
+            .doc(roomId)
+            .onSnapshot((snapshot) => setRoomDetails(snapshot.data()))
+        }
+    }, [roomId]);
+
+    console.log(roomDetails);
+
   return (
     <div className="chat">
         <div className="chat__header">
             <div className="chat__headerLeft">
               <h4 className="chat__channelName">
-                  <strong>#general</strong>
+                  <strong>#{roomDetails?.name}</strong>
                   <StarBorderOutlined />
               </h4>
             </div>
