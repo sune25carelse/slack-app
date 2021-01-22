f10447705a2f1a97980d6c1327bbfb8473d9f16e
Sune Carelse <sune.carelse@takealot.com>
2021-01-22 20:31:17 +0200

    state for authentication  login

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from './Chat';
+import React, { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="App">
       <Router>
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Welcome</h1>
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <h1>LOGIN PAGE</h1>
+        ): (
+         <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+             <Switch>
+              <Route path="/room/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <h1>Welcome</h1>
+              </Route>
+             </Switch>
+          </div>
+        </>
+        )}
       </Router>
     </div>
   );
