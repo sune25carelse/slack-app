8bf59eb8832ce62130465299a21ff323547ae130
Sune Carelse <sune.carelse@takealot.com>
2021-01-22 22:00:28 +0200

    created the dataLayer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from './Chat';
-import React, { useState } from 'react';
+import { useStateValue } from './StateProvider';
 import Login from './Login';
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [{ user }, dispatch] = useStateValue();
 
   return (
     <div className="App">
